9017ec41c381ab042c4c5dc3f3289745ddb688b3
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-20 12:08:56 +0100

    Bump version to 4.49.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 94
-        versionName = "4.49.0"
+        versionCode = 95
+        versionName = "4.49.1"
     }
 
     // Stable signing support.
